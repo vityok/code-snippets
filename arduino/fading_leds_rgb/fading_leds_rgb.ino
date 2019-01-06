a55e7d246f75062ba3a74b5483e04de4f40631e5
vityok <vityok@localhost>
2019-01-06 22:49:49 +0200

    a new mode for fading leds

---
+++ arduino/fading_leds_rgb/fading_leds_rgb.ino
@@ -0,0 +1,61 @@
+// -*- mode: c; -*-
+
+// Gradually changes brightness of LEDs (fades and brighter) connected
+// to analog out pins
+
+// number of LEDs
+const int GROUPS_COUNT = 3;
+// pins that these LEDs are connected to
+const int pins[3][2] = 
+{{3, 5},
+		      {6, 9},
+		      {10, 11}};
+
+// value output to the pin
+int values[GROUPS_COUNT];
+// change between cycles
+int deltas[GROUPS_COUNT];
+
+void setGroup(int group, int value) {
+  values[group] = value;
+  analogWrite(pins[group][0], value);
+  analogWrite(pins[group][1], value);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i < GROUPS_COUNT; ++i) {
+    pinMode(pins[i][0], OUTPUT);
+    pinMode(pins[i][1], OUTPUT);
+    setGroup(i, 205);
+    deltas[i] = 5 + random(5);
+    delay(500);
+    setGroup(i, 0);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int group = 0; group < GROUPS_COUNT; ++group) {
+    int val = 0;
+    while (val < 255) {
+      setGroup(group, val);
+      val += 5;
+      // wait for N milliseconds to see the dimming effect
+      delay(100);
+    }
+    val = 255;
+    setGroup(group, val);
+    delay(100);
+
+    while (val > 0) {
+      setGroup(group, val);
+      val -= 5;
+      // wait for N milliseconds to see the dimming effect
+      delay(100);
+    }
+    setGroup(group, 0);
+    // wait for N milliseconds to see the dimming effect
+    delay(100);
+  }
+}
