2a2c438ef7f1f87bbda7bf84ecb8d338897e0023
vityok <vityok@localhost>
2019-02-04 23:49:53 +0200

    added exception experiment in C#

---
+++ exceptions/cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+	Exception ExceptionGenerator()
+	{
+	    // despite being created here, the "throw" keyword defines
+	    // line number of the frame 0. There will be no way to
+	    // find out where this exception was created, only where
+	    // it was thrown
+	    return new Exception();
+	}
+
+
+	void ExceptionThrower() {
+	    // the line below is stored in the thrown excpetion stack
+	    // trace at frame 0
+	    throw ExceptionGenerator();
+	}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	    Program p = new Program();
+	    p.ExceptionThrower();
+        }
+    }
+}
