471396ca78aad46a7b671d7e5ee96f6151ea4927
vityok <vityok@localhost>
2019-02-11 22:34:37 +0200

    initial attempt to check how calling GC.WaitForPendingFinalizers() from a finalizer works

---
+++ gc/cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace cs
+{
+
+    class CefEngine
+    {
+	public void Shutdown()
+	{
+            Console.WriteLine("Shutdown()");
+	    GC.Collect();
+	    GC.WaitForPendingFinalizers();
+	}
+    }
+
+    class Config
+    {
+	CefEngine engine = new CefEngine();
+
+	~Config()
+	{
+	    engine.Shutdown();
+            Console.WriteLine("after Shutdown");
+	}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    Config c;
+            Console.WriteLine("Hello World!");
+	    for (var i = 0; i < 100; ++i) {
+		c = new Config();
+	    }
+	    // c = null;
+	    // GC.Collect();
+            Console.WriteLine("DONE!");
+        }
+    }
+}
