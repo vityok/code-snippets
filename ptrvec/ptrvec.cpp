ccf192573ac5bc01d2aab80b83b0622e8737cba3
vityok <vityok@localhost>
2019-02-16 13:42:59 +0200

    first benchmark results, some improvements

--- ptrvec/ptrvec.cpp
+++ ptrvec/ptrvec.cpp
@@ -3,30 +3,108 @@
 #include <memory>
 #include <vector>
 #include <iostream>
+#include <iterator>
 
-int square(int num)
+#include <benchmark/benchmark.h>
+
+using vec_type = std::vector<std::unique_ptr<int>>;
+
+const vec_type::size_type SIZE = 1000;
+
+static void append_pushing(benchmark::State& state)
 {
-    std::vector<std::unique_ptr<int>> vec;
+    for (auto _ : state)
+    {
+	state.PauseTiming(); // Stop timers. They will not count until they are resumed.
+	vec_type src;
+	vec_type dst;
+	benchmark::DoNotOptimize(src.data());
+	benchmark::DoNotOptimize(dst.data());
 
-    for (auto i = 0; i < 10; ++i) {
-        auto tmp = std::make_unique<int>(i);
-        vec.push_back(std::move(tmp));
+	// create source vector
+	for (vec_type::size_type i = 0; i < SIZE; ++i)
+	{
+	    src.push_back(std::make_unique<int>(i));
+	}
+	state.ResumeTiming(); // And resume timers. They are now counting again.
+
+	// append it to the destination vector
+	for (vec_type::size_type i = 0; i < src.size(); ++i)
+	{
+	    dst.push_back(std::move(src[i]));
+	}
+
+	if (dst.size() != SIZE) { abort(); }
     }
-
-    for (auto i = 0; i < vec.size(); ++i) {
-	std::cout << *(vec[i]) << ", ";
-    }
-    std::cout << std::endl;
-
-    return num * num;
 }
+BENCHMARK(append_pushing);
 
+static void append_emplacing(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+	state.PauseTiming(); // Stop timers. They will not count until they are resumed.
+	vec_type src;
+	vec_type dst;
+	benchmark::DoNotOptimize(src.data());
+	benchmark::DoNotOptimize(dst.data());
+
+	// create source vector
+	for (vec_type::size_type i = 0; i < SIZE; ++i)
+	{
+	    src.emplace_back(std::make_unique<int>(i));
+	}
+	state.ResumeTiming(); // And resume timers. They are now counting again.
+
+	// append it to the destination vector
+	for (vec_type::size_type i = 0; i < src.size(); ++i)
+	{
+	    dst.emplace_back(std::move(src[i]));
+	}
+
+	if (dst.size() != SIZE) { abort(); }
+    }
+}
+BENCHMARK(append_emplacing);
+
+static void append_moving(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+	state.PauseTiming(); // Stop timers. They will not count until they are resumed.
+	vec_type src;
+	vec_type dst;
+	src.reserve(SIZE+1);
+	dst.reserve(SIZE+1);
+	benchmark::DoNotOptimize(src.data());
+	benchmark::DoNotOptimize(dst.data());
+	// create source vector
+	for (vec_type::size_type i = 0; i < SIZE; ++i)
+	{
+	    src.push_back(std::make_unique<int>(i));
+	}
+	state.ResumeTiming(); // And resume timers. They are now counting again.
+
+	// append it to the destination vector
+	std::move(std::begin(src), std::end(src), std::back_inserter(dst));
+
+	if (dst.size() != SIZE) { abort(); }
+    }
+}
+BENCHMARK(append_moving);
+
+BENCHMARK_MAIN();
+/*
 int main()
 {
-    std::cout << square(10) << std::endl;
+    append_pushing();
+    append_emplacing();
+    append_moving();
+    
     return 0;
 }
+*/
 
-// g++ -Wall -pedantic -o ptrvec -std=c++14 ptrvec.cpp
+// g++ $(< build.gcc.opt)
 // clang -o ptrvec -std=c++14 ptrvec.cpp
 
